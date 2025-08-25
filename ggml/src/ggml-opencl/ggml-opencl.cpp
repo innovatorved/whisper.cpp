582ef379ab3c7f02305fd3ddf914ba68052c301d
lhez <lih@qti.qualcomm.com>
2025-08-25 14:18:09 -0700

    opencl: fix support ops condition for `rms_norm` (llama/15560)

--- ggml/src/ggml-opencl/ggml-opencl.cpp
+++ ggml/src/ggml-opencl/ggml-opencl.cpp
@@ -2647,8 +2647,9 @@ static bool ggml_opencl_supports_op(ggml_backend_dev_t dev, const struct ggml_te
             return op->src[0]->type == GGML_TYPE_F32;
         case GGML_OP_SOFT_MAX:
         case GGML_OP_NORM:
-        case GGML_OP_RMS_NORM:
             return true;
+        case GGML_OP_RMS_NORM:
+            return op->ne[0] % 4 == 0 && ggml_is_contiguous_rows(op->src[0]);
         case GGML_OP_REPEAT:
             return op->src[0]->type == GGML_TYPE_F32 && op->type == GGML_TYPE_F32; // Assuming F32 for now, can be expanded
         case GGML_OP_PAD:
